2ecb4b0aca7770bf72cd166f2f2494703c8a230d
Narachii <naoya706oxford@gmail.com>
2021-10-25 18:46:44 +0900

    Implement hn application

--- main.go
+++ main.go
@@ -1,21 +1,88 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/Narachii/quiet_hn/hn"
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
 )
 
 func main() {
-	var client hn.Client
-	ids, err := client.TopItems()
-	if err != err {
-		panic(err)
-	}
-	for i := 0; i < 5; i++ {
-		item, err := client.GetItem(ids[i])
+	// parse flags
+	var port, numStories int
+	flag.IntVar(&port, "port", 3000, "the port to start the web server on")
+	flag.IntVar(&numStories, "stories", 30, "the number of top stories to display")
+	flag.Parse()
+
+	tpl := template.Must(template.ParseFiles("./index.gohtml"))
+
+	http.HandleFunc("/", handler(numStories, tpl))
+
+	//Start the server
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
+
+func handler(numStories int, tpl *template.Template) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		var client hn.Client
+		ids, err := client.TopItems()
 		if err != nil {
-			panic(err)
+			http.Error(w, "Failed to load top stories", http.StatusInternalServerError)
+			return
 		}
-		fmt.Printf("%s (by%s)\n", item.Title, item.By)
+
+		var stories []item
+		for _, id := range ids {
+			hnItem, err := client.GetItem(id)
+			if err != nil {
+				continue
+			}
+			item := parseHNItem(hnItem)
+			if isStoryLink(item) {
+				stories = append(stories, item)
+				if len(stories) >= numStories {
+					break
+				}
+			}
+		}
+		data := templateData{
+			stories,
+			time.Now().Sub(start),
+		}
+		err = tpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, "Failde to process the template", http.StatusInternalServerError)
+			return
+		}
+	})
+}
+
+func isStoryLink(item item) bool {
+	return item.Type == "story" && item.URL != ""
+}
+
+func parseHNItem(hnItem hn.Item) item {
+	ret := item{Item: hnItem}
+	url, err := url.Parse(ret.URL)
+	if err == nil {
+		ret.Host = strings.TrimPrefix(url.Hostname(), "www")
 	}
+	return ret
+}
+
+// item is the same as the hn.Item, but adds the Host field
+type item struct {
+	hn.Item
+	Host string
+}
+
+type templateData struct {
+	Stories []item
+	Time    time.Duration
 }
